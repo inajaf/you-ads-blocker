5d4b7fab1cc10b65ca6ae15f5921896795d9f5ce
Eldar Najafov <najaf.eldar@gmail.com>
2026-07-22 00:23:21 +0400

    Fix Ko-fi widget loading by adding script to landing.html

--- src/landing/KofiWidget.tsx
+++ src/landing/KofiWidget.tsx
@@ -1,31 +1,8 @@
-import { useEffect, useRef } from 'react'
-
 export function KofiWidget() {
-  const containerRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const container = containerRef.current
-    if (!container) return
-
-    const script = document.createElement('script')
-    script.src = 'https://storage.ko-fi.com/cdn/widget/Widget_2.js'
-    script.async = true
-    script.onload = () => {
-      if (typeof window.kofiwidget2 !== 'undefined') {
-        window.kofiwidget2.draw()
-      }
-    }
-    container.appendChild(script)
-
-    return () => {
-      script.remove()
-    }
-  }, [])
-
   return (
     <div className="nv-kofi" data-reveal>
       <div className="nv-kofi-inner">
-        <div id="kofi-widget" ref={containerRef} />
+        <div id="kofi-widget" />
       </div>
     </div>
   )
