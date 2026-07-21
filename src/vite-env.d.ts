66c9b510654016c5716d624c39d393bdbfefbd95
Eldar Najafov <najaf.eldar@gmail.com>
2026-07-22 00:13:18 +0400

    Add Ko-fi support widget to landing page

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
+
+interface KofiWidget {
+  init(text: string, color: string, id: string): void
+  draw(): void
+}
+
+interface Window {
+  kofiwidget2: KofiWidget
+}
