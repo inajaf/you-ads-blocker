c58073a275ee29d2995ef92466489bfb6f2335ad
Eldar Najafov <najaf.eldar@gmail.com>
2026-08-02 14:39:21 +0400

    feat(android): remove protection header, always-on ad blocking

--- android/AdVoid/app/src/main/java/com/advoid/app/PlaybackUiCoordinator.kt
+++ android/AdVoid/app/src/main/java/com/advoid/app/PlaybackUiCoordinator.kt
@@ -1,14 +1,12 @@
 package com.advoid.app
 
 data class PlaybackUiState(
-    val headerHidden: Boolean,
     val keepScreenOn: Boolean,
 )
 
 class PlaybackUiCoordinator {
     private var activityVisible = false
     private var videoPlaying = false
-    private var fullscreen = false
 
     fun onActivityVisibilityChanged(visible: Boolean): PlaybackUiState {
         activityVisible = visible
@@ -20,14 +18,8 @@ class PlaybackUiCoordinator {
         return currentState()
     }
 
-    fun onFullscreenChanged(active: Boolean): PlaybackUiState {
-        fullscreen = active
-        return currentState()
-    }
-
     fun currentState(): PlaybackUiState {
         return PlaybackUiState(
-            headerHidden = activityVisible && (videoPlaying || fullscreen),
             keepScreenOn = activityVisible && videoPlaying,
         )
     }
